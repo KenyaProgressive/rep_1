8809308e1e9b4ddee8f152d107d8c48a695b3df0
KenyaProgressive <ya.nikita-parfenov@yandex.ru>
2025-02-19 22:01:55 +0300

    commit1 - validation of IP

---
+++ ping.h
@@ -0,0 +1,12 @@
+#ifndef PING_H
+#define PING_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int input_address();
+int validate_address(char *str);
+
+#endif
